c36f3a7b6711f3ace523d7a06caab8da39c2e609
Samyak <samyakdarshan97@gmail.com>
2020-09-14 12:26:30 +0530

    just entering merge conflict

--- LTIRegistrationApp/src/HelloWorld.java
+++ LTIRegistrationApp/src/HelloWorld.java
@@ -3,7 +3,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World");
-		System.out.println("No hello");
+		System.out.println("Why hello?");
 		// TODO Auto-generated method stub
 
 	}
